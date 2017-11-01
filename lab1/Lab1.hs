b5d095d3c79f665b89d48b97e576973ddc6e42a8
Kristoffer Ek <kristofferboek@gmail.com>
2017-11-01 15:56:28 +0100

    Lab1 done

--- lab1/Lab1.hs
+++ lab1/Lab1.hs
@@ -1,9 +1,4 @@
 import Test.QuickCheck
--- Implementation of the power function from the lecture
-power :: Integer -> Integer -> Integer
-power n k | k < 0 = error "power: negative argument"
-power n 0 = 1
-power n k = n * power n (k-1)
 
 {- Part 1
   In order to calculate n^k, k+1 computing "steps" are used.
@@ -19,3 +14,25 @@ power1 n k = product [n | a <- [1..k]]
 prop_power1 :: Integer -> Integer -> Bool
 prop_power1 n k = power1 n k' == n^k'
   where k' = abs k
+
+-- Part 3
+power2 :: Integer -> Integer -> Integer
+power2 n k | k == 0            = 1
+           | k == 1            = n
+           | k `mod` 2 == 0    = power2 (n*n) (k `div` 2)
+           | k `mod` 2 == 1    = n * (power2 n (k-1))
+
+prop_power2 :: Integer -> Integer -> Bool
+prop_power2 n k = power2 n k' == n^k'
+  where k' = abs k
+
+-- Part 4
+
+prop_power :: Integer -> Integer -> Bool
+prop_power n k = (power1 n k' == power2 n k') && (power1 n k' == n^k')
+  where k' = abs k
+{-
+Instead of comming up with our own test cases we use QuickCheck.
+QuickCheck works becasue we have added "k' = abs k", which makes sure that we
+doesn't test for negative values of k.
+-}
