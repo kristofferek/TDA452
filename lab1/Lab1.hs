031cbd28a5bb4aa1f767bfaf9dff3b7347eace72
Kristoffer Ek <kristofferboek@gmail.com>
2017-11-01 14:56:12 +0100

    Part 2 done

--- lab1/Lab1.hs
+++ lab1/Lab1.hs
@@ -1,3 +1,4 @@
+import Test.QuickCheck
 -- Implementation of the power function from the lecture
 power :: Integer -> Integer -> Integer
 power n k | k < 0 = error "power: negative argument"
@@ -11,4 +12,10 @@ power n k = n * power n (k-1)
 
 -- Part 2
 power1 :: Integer -> Integer -> Integer
-power1 n k
+power1 n k | k < 0 = error "power: negative argument"
+power1 n 0 = 1
+power1 n k = product [n | a <- [1..k]]
+
+prop_power1 :: Integer -> Integer -> Bool
+prop_power1 n k = power1 n k' == n^k'
+  where k' = abs k
