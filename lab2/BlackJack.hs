845d1cad21dc1c172df571a99829b85807bb824a
Kristoffer Ek <kristofferboek@gmail.com>
2017-11-06 16:01:21 +0100

    Started on the value func

--- lab2/BlackJack.hs
+++ lab2/BlackJack.hs
@@ -2,16 +2,35 @@ module BlackJack where
 import Cards
 import RunGame
 
+{- Task 3.1
+  We have read the document carefully
+-}
+
+{- Task 3.2
+size (Add (Card (Numeric 2) Hearts)
+        (Add (Card Jack Spades) Empty))
+
+Step 1) size will match the hand with the followin pattern "(Add card hand)",
+        where card in this case = "(Card (Numeric 2) Hearts))"
+        and hand = (Add (Card Jack Spades) Empty)
+
+Step 2) size will, as of now, return "1 + size hand"
+        hand is not empty so pattern "(Add card hand)" will match.
+        card = (Card Jack)
+-}
+
 -- Returns an empty hand
 empty :: Hand
 empty = Empty
 
---
+-- Returns the total value of a hand
 value :: Hand -> Integer
 value Empty = 0
-value (Add c h) = rank c + value h
+value (Add (Card (Numeric i) _) h) = i + value h
+value (Add (Card r _) h) | r == Ace = 11 + value h
+value (Add _ h) = 10 + value h
 
-card1 = Card Numeric 3 Spades
-card2 = Card Numeric 5 Hearts
-
-hand = Add card1 Empty
+-- Example hand
+card1 = Card (Numeric 3) Spades
+card2 = Card Ace Hearts
+hand = Add card2 (Add card1 (Add card2 Empty))
