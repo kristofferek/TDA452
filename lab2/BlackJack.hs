105056fbaedb3cce455fe56a7b828e6f4041cfe8
Kristoffer Ek <kristofferboek@gmail.com>
2017-11-13 18:33:56 +0100

    I think Part B is done now

--- lab2/BlackJack.hs
+++ lab2/BlackJack.hs
@@ -85,6 +85,7 @@ gameOver = (> 21) . value
 
 -- Which player is the winner? Bank or Guest?
 -- Guest is the first parameter
+winner :: Hand -> Hand -> Player
 winner g b
   | gameOver g               = Bank
   | gameOver b               = Guest
@@ -93,24 +94,29 @@ winner g b
 
 
 
-
 --                  PART B
 
+
+-- Given two hands, <+ puts the first one on top of the second one
 (<+) :: Hand -> Hand -> Hand
 (<+) Empty h2          = h2
 (<+) (Add c1 Empty) h2 = Add c1 h2
 (<+) (Add c1 h1)    h2 = Add c1 (h1<+h2)
 
+-- Checks that (<+) is associative
 prop_onTopOf_assoc :: Hand -> Hand -> Hand -> Bool
 prop_onTopOf_assoc p1 p2 p3 = p1<+(p2<+p3) == (p1<+p2)<+p3
 
+-- Checks that (<+) doesn't change the size of the hand
 prop_size_onTopOf :: Hand -> Hand -> Bool
 prop_size_onTopOf h1 h2 = (size (h1<+h2)) == (size h1 + size h2)
 
+-- Returns a full pack of cards. One of each, not shuffled
 fullDeck :: Hand
 fullDeck = (suitDeck Hearts)<+(suitDeck Spades)
               <+(suitDeck Diamonds)<+(suitDeck Clubs)
 
+-- Returns a hand with rank 2-Ace in a specific suit (13 cards)
 suitDeck :: Suit -> Hand
 suitDeck s = suitDeck' s 2
   where
@@ -121,11 +127,14 @@ suitDeck s = suitDeck' s 2
       | i == 13   = (Add (Card King s) (suitDeck' s (i+1)))
       | otherwise = (Add (Card Ace s) Empty)
 
-
+-- Draws the first card from a deck and adds it to a hand.
+-- Returns (deck, hand)
 draw :: Hand -> Hand -> (Hand,Hand)
 draw Empty hand = error "draw: The deck is empty."
 draw (Add c h) hand = ( h, Add c hand )
 
+-- Plays as the bank and returns the final hand.
+-- Draws a new card as long as the value is less than 16
 playBank :: Hand -> Hand
 playBank deck = playBank' deck Empty
   where
@@ -135,13 +144,16 @@ playBank deck = playBank' deck Empty
         where
           (deck',bankHand') = draw deck bankHand
 
+-- Shuffles the order of a given deck
 shuffle :: StdGen -> Hand -> Hand
 shuffle g Empty = Empty
 shuffle g deck = Add rc (shuffle g' rdeck)
   where
-    (n, g') = randomR (0, size deck-1) g
-    (rc, rdeck) = drawNthCard deck n
+    (n, g') = randomR (0, size deck-1) g -- Gets a random index of the deck
+    (rc, rdeck) = drawNthCard deck n -- rc = n'th card, rdeck = resulting deck
 
+-- Draws the n'th card from a deck.
+-- Returns (card, deck), where deck is the deck without the card.
 drawNthCard :: Hand -> Integer -> (Card, Hand)
 drawNthCard deck n = drawNthCard' Empty deck n 0
   where
@@ -149,17 +161,21 @@ drawNthCard deck n = drawNthCard' Empty deck n 0
       | n == i    = (c, (before<+after))
       | otherwise = drawNthCard' (Add c before) after n (i+1)
 
+-- Checks if a card is still in the deck after it has been shuffled
 prop_shuffle_sameCards :: StdGen -> Card -> Hand -> Bool
 prop_shuffle_sameCards g c h =
     c `belongsTo` h == c `belongsTo` shuffle g h
 
+-- Is a given Card in the hand?
 belongsTo :: Card -> Hand -> Bool
 c `belongsTo` Empty = False
 c `belongsTo` (Add c' h) = c == c' || c `belongsTo` h
 
+-- Checks if a deck has the same size after it has been shuffled
 prop_size_shuffle :: StdGen -> Hand -> Bool
 prop_size_shuffle g hand = size hand == size (shuffle g hand)
 
+-- Prints a hand in a more readable way
 printHand :: Hand -> IO ()
 printHand hand = putStr $ unlines (printHand' hand)
   where
