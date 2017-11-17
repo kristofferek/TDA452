1bdd2f6b542c9c13d02adfec79aabfdcb9558746
longnktx <yh_long.kotex@yahoo.com.vn>
2017-11-17 15:31:26 +0100

    printSudoku done

--- lab3/Sudoku.hs
+++ lab3/Sudoku.hs
@@ -15,22 +15,25 @@ example = Sudoku
     n = Nothing
     j = Just
 
+-- Create a 9x9 blank sudoku
+-- number of rows and collumns can be changed
+
 allBlankSudoku :: Sudoku
-allBlankSudoku = Sudoku (blankSudoku 9 9 [])
-  where blankSudoku x y list = if x > 0 then blankSudoku (x-1) y ((createRow y []):list) else list
-        createRow y row = if y > 0 then createRow (y-1) (n:row) else row
+allBlankSudoku = Sudoku (blankSudoku 9 9)
+  where blankSudoku x y = replicate x (replicate y n)
         n = Nothing
 
-isSudoku :: Sudoku -> Bool
-isSudoku (Sudoku s) = check9Collums(s) && (length s == 9) && (all (\x -> checkAllValue x) s)
+-- Check if input is a legit sudoku
 
-check9Collums :: [[Maybe Int]] -> Bool
-check9Collums s = all (\x -> length x == 9) s
+isSudoku :: Sudoku -> Bool
+isSudoku (Sudoku s) = (all (\x -> length x == 9) s) && (length s == 9) && (all (\x -> checkAllValue x) s)
 
 checkAllValue :: [Maybe Int] -> Bool
 checkAllValue list = all (\x -> checkOneValue x) list
   where checkOneValue (Just x) = if x > 0 && x < 10 then True else False
         checkOneValue Nothing = True
+
+-- Check if the sudoku is filled with numbers
 
 isFilled :: Sudoku -> Bool
 isFilled (Sudoku s) = all (\x -> checkAllFilled x) s
@@ -39,3 +42,12 @@ checkAllFilled :: [Maybe Int] -> Bool
 checkAllFilled list = all (\x -> checkOneFilled x) list
   where checkOneFilled Nothing = False
         checkOneFilled _ = True
+
+printSudoku :: Sudoku -> IO()
+printSudoku (Sudoku s) = putStrLn (concat (map oneRowToStr s))
+
+oneRowToStr :: [Maybe Int] -> String
+oneRowToStr [] = "\n"
+oneRowToStr (x:xs) = (maybeToStr x) ++ oneRowToStr xs
+  where maybeToStr (Just x) = show x
+        maybeToStr Nothing ="."
