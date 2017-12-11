8df58fa6f9078f783aec8465f6d529a21d46a4bc
Kristoffer Ek <kristofferboek@gmail.com>
2017-12-11 13:26:10 +0100

    Some ui stuff

--- lab4/MineSweeper.hs
+++ lab4/MineSweeper.hs
@@ -1,21 +1,62 @@
+import System.Random
+import System.Console.ANSI
+import Prelude hiding (Either(..))
+
 data Tile = Mine | Numeric Int deriving Eq
 data Status = Hidden | Opened deriving (Eq,Show)
 data Cell = Cell {status :: Status, tile :: Tile}
 type Pos = (Int,Int)
 type Board = [[Cell]]
+type Coord = (Int, Int)
+data Input = Up
+           | Down
+           | Left
+           | Right
+           | Exit
+           deriving (Eq,Show)
+
+
+main :: IO()
+main = do
+  --board <- makeBoard
+  --printBoard board
+  inp <- getInput
+  print inp
 
 instance Show Tile where
-  show Mine = "*"
+  show Mine = "💥"
   show (Numeric i) = show i
 
 instance Show Cell where
   show (Cell Hidden _) = "."
   show (Cell _ tile) = show tile
 
+dirToCoord :: Input -> Coord
+dirToCoord d
+  | d == Up    = (0, -1)
+  | d == Down  = (0,  1)
+  | d == Left  = (-1, 0)
+  | d == Right = (1,  0)
+  | otherwise  = (0,  0)
+
+getInput :: IO Input
+getInput = do
+  char <- getChar
+  case char of
+    'q' -> return Exit
+    'w' -> return Up
+    's' -> return Down
+    'a' -> return Left
+    'd' -> return Right
+    _ -> getInput
+
 allBlankBoard :: Board
-allBlankBoard = replicate 9 (replicate 9 (Cell Opened (Numeric 0)))
+allBlankBoard = replicate 9 (replicate 9 (Cell Opened Mine))
 
 printBoard :: Board -> IO ()
 printBoard board = putStrLn $ concat [printBoard' x | x <- board]
   where printBoard' [] = "\n"
         printBoard' (x:xs) = show x ++ " " ++ printBoard' xs
+
+(|+|) :: Coord -> Coord -> Coord
+(|+|) (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
